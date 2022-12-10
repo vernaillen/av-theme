4a8d17a6c4c304b90e20d99db6d9e3594c9aaa80
Wouter Vernaillen <wouter@vernaillen.com>
2022-12-10 14:09:03 +0100

    use twicpics for playground

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -2,5 +2,14 @@ export default defineNuxtConfig({
   extends: '..',
   typescript: {
     includeWorkspace: true
-  }
+  },
+  image: {
+    provider: 'twicpics',
+    twicpics: {
+      baseURL: 'https://vernaillen.twic.pics/anneleen',
+    },
+  },
+  generate: {
+    routes: ['/', '/test', '/markdown-cheat-sheet', '/alfa'],
+  },
 })
