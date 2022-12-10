f8245172e3f9f39ad88f73a3d28cbe261c491b3d
Wouter Vernaillen <wouter@vernaillen.com>
2022-12-10 13:48:12 +0100

    more documentation

--- .docs/app.config.ts
+++ .docs/app.config.ts
@@ -1,5 +1,29 @@
 export default defineAppConfig({
   docus: {
-    title: 'av-theme documentatie'
+    title: 'av-theme',
+    socials: {
+      github: 'vernaillen/av-theme',
+    },
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: false
+    },
+    aside: {
+      level: 1,
+      filter: [],
+    },
+    header: {
+      title: false,
+      logo: false,
+      showLinkIcon: true
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'av-theme created by Wouter Vernaillen',
+        href: 'https://vernaillen.dev',
+      }
+    }
   }
 })
--- .playground/content/index.md
+++ .playground/content/index.md
@@ -5,4 +5,10 @@ image:
   src: 'header-imgs/header-test1.png'
 ---
 
-# Home
+# av-theme
+
+Test website voor het [av-theme](https://github.com/vernaillen/av-theme)
+
+## documentatie
+
+[vind je hier](https://av-theme-docs.netlify.app/)
